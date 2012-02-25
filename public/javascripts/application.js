20808d5e388df00d20a140f6a59081f8ed719ea4
Cary FitzHugh <cary.fitzhugh@ziplist.com>
2012-02-24 23:30:03 -0500

    autoquery - does a query, and populates a div with response.
    i think this is what Matt wanted - and specific functionality can be
    achieved by changing what is returned by the server.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -18,6 +18,7 @@ function initialize_js(root) {
   find("*[data-sticky-header]").mx_sticky_header();
   find("*[data-sortable-table]").mx_sortable_table();
   find("*[data-save-warning]").mx_save_warning();
+  find("*[data-autoquery]").mx_autoquery();
 }
 
 
@@ -40,7 +41,40 @@ function mx_replace(element, content) {
   $(element).replaceWith(content);
   initialize_js($(element));
 }
+
 function mx_update(element, content) {
   $(element).html(content);
   initialize_js($(element));
 }
+
+// parseUri 1.2.2
+// (c) Steven Levithan <stevenlevithan.com>
+// MIT License
+function parseUri (str) {
+	var	o   = parseUri.options,
+		m   = o.parser[o.strictMode ? "strict" : "loose"].exec(str),
+		uri = {},
+		i   = 14;
+
+	while (i--) uri[o.key[i]] = m[i] || "";
+
+	uri[o.q.name] = {};
+	uri[o.key[12]].replace(o.q.parser, function ($0, $1, $2) {
+		if ($1) uri[o.q.name][$1] = $2;
+	});
+
+	return uri;
+};
+
+parseUri.options = {
+	strictMode: false,
+	key: ["source","protocol","authority","userInfo","user","password","host","port","relative","path","directory","file","query","anchor"],
+	q:   {
+		name:   "queryKey",
+		parser: /(?:^|&)([^&=]*)=?([^&]*)/g
+	},
+	parser: {
+		strict: /^(?:([^:\/?#]+):)?(?:\/\/((?:(([^:@]*)(?::([^:@]*))?)?@)?([^:\/?#]*)(?::(\d*))?))?((((?:[^?#\/]*\/)*)([^?#]*))(?:\?([^#]*))?(?:#(.*))?)/,
+		loose:  /^(?:(?![^:@]+:[^:@\/]*@)([^:\/?#.]+):)?(?:\/\/)?((?:(([^:@]*)(?::([^:@]*))?)?@)?([^:\/?#]*)(?::(\d*))?)(((\/(?:[^?#](?![^?#\/]*\.[^?#\/.]+(?:[?#]|$)))*\/?)?([^?#\/]*))(?:\?([^#]*))?(?:#(.*))?)/
+	}
+};
