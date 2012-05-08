dd253d2caff063317a8c15b5464ff257d60eafd0
Sandra Hall <sandrajhall@gmail.com>
2012-05-08 12:35:59 -0400

    added new fields to ce form, list, show

--- lib/trait.rb
+++ lib/trait.rb
@@ -5,12 +5,12 @@ module Trait
     tn_txt = taxon_name.nil? ? 'Taxon Name Not Provided' : taxon_name.cached_display_name.html_safe
     # TODO: fix me
     ce_txt = ce.nil? ? 'Study/Population Not Provided' : Trait.trait_ce_name(ce)
-    s = [ref_txt, tn_txt, ce_txt].join(" : ").html_safe
+    s = [tn_txt, ref_txt, ce_txt].join("_").html_safe
     s.truncate(254)
   end
 
   def self.trait_ce_name(ce)
-   s = [ ce.geography, ce.sd_y, ce.ed_y, ce.population ].compact.join(":")
+   s = [ ce.geography+ce.sd_y+'-'+ce.ed_y, ce.population ].compact.join("_")
    s 
   end
 
