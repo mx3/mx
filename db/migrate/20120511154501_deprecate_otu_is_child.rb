e926ee3abedef833a210364d24849041bea53b4f
mjy <diapriid@gmail.com>
2012-05-20 17:28:43 -0500

    Undeprecate otu_is_child based on discussion with those using it

--- db/migrate/20120511154501_deprecate_otu_is_child.rb
+++
@@ -1,9 +0,0 @@
-class DeprecateOtuIsChild < ActiveRecord::Migration
-  def self.up
-    remove_column :otus, :is_child
-  end
-
-  def self.down
-    add_column :otus, :is_child, :boolean  
-  end
-end
